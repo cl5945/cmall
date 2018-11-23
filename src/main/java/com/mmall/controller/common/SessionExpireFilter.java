f33b5d6d547c1460ee38e568369023a5abb96045
unknown <cjh_945@163.com>
2018-11-23 15:42:09 +0800

    sessionExpireFiler

--- src/main/java/com/mmall/controller/common/SessionExpireFilter.java
+++ src/main/java/com/mmall/controller/common/SessionExpireFilter.java
@@ -1,7 +1,44 @@
 package com.mmall.controller.common;
 
+import com.mmall.common.Const;
+import com.mmall.pojo.User;
+import com.mmall.util.CookieUtil;
+import com.mmall.util.JsonUtil;
+import com.mmall.util.RedisPoolUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * Created by changlei on 2018/11/23.
  */
-public class SessionExpireFilter {
+public class SessionExpireFilter extends OncePerRequestFilter {
+    /**
+     * Same contract as for {@code doFilter}, but guaranteed to be
+     * just invoked once per request within a single request thread.
+     * See {@link #shouldNotFilterAsyncDispatch()} for details.
+     * <p>Provides HttpServletRequest and HttpServletResponse arguments instead of the
+     * default ServletRequest and ServletResponse ones.
+     *
+     * @param request
+     * @param response
+     * @param filterChain
+     */
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String loginToken = CookieUtil.readLoginToken(request);
+        if(StringUtils.isNotBlank(loginToken)){
+            String userJson = RedisPoolUtil.get(loginToken);
+            User user = JsonUtil.string2Obj(userJson, User.class);
+            if(user != null){
+                RedisPoolUtil.expire(loginToken,Const.RedisCacheExtime.REDIS_SESSION_EXTIME);
+            }
+        }
+        filterChain.doFilter(request,response);
+    }
 }
